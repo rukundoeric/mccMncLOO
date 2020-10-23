1b02410a75533d1701e349c476b03697c205b9e7
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-10-23 17:56:42 +0200

    Initialize server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import logger from 'morgan';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import api from './api/routes/index';
+
+const app = express();
+const port = process.env.PORT || 3456;
+
+dotenv.config();
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(cors())
+  .use('/api', api)
+  .use((_, res) => {
+    res.status(404).send({
+      status: 404,
+      error: {
+        message: 'Page Not found!',
+      }
+    });
+  });
+
+app.set('port', port);
+app.use(logger('dev'));
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
